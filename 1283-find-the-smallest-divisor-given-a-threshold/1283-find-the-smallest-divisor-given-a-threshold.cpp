348813ef968e193c0fb8d9138573aef4d254ec34
KAUSTUBH24SHATDHAR <103636236+KAUSTUBH24SHATDHAR@users.noreply.github.com>
2023-08-22 20:12:18 +0530

    Time: 39 ms (73.88%), Space: 35.5 MB (6.39%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+    
+    int findmax(vector<int>nums){
+        int max=INT_MIN;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(max<nums[i]){
+                max=nums[i];
+            }
+            
+        }
+        
+        return max;
+     
+    }
+    
+    
+  int findsum(vector<int>nums, int mid){
+        
+        int sum=0;
+        
+        for(int i=0;i<nums.size();i++){
+        float x = (float)nums[i]/mid;
+            sum+=ceil(x);        
+        }
+        
+        return sum;
+    }
+
+        
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        
+        int s=1;
+        int e=findmax(nums);
+        
+        int ans=-1;
+        
+        while(s<=e){
+            
+        
+        int mid=(s+e)/2;
+        
+       int sum = findsum(nums,mid);
+        
+        if(sum<=threshold){
+            ans=mid;
+            e=mid-1;
+        }
+
+        
+        else{
+            s=mid+1;
+        }
+       
+      }
+        
+        return ans;
+    }
+};
+// class Solution {
+// public:
+
+//     int SumOfDivisor(vector<int>& nums, int Day){
+//         int sum = 0;
+//         for(int i = 0;i<nums.size();i++){
+//             sum += ceil((double)nums[i]/(double)(Day));
+//         }
+//         return sum;
+//     }
+//     int smallestDivisor(vector<int>& nums, int threshold) {
+//         int maxi = INT_MIN;
+//         for(int i = 0;i<nums.size();i++){
+//             maxi = max(maxi,nums[i]);
+//         }
+//         int low = 1,high = maxi,ans = -1;
+//         while(low <= high){
+//             int mid = (low + high)/2;
+//             if(SumOfDivisor(nums,mid) <= threshold){
+//                 ans = mid;
+//                 high = mid - 1;
+//             }
+//             else{
+//                 low = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
