f7563a6e912cd0efe224d9f314c3296220531fcd
KAUSTUBH24SHATDHAR <103636236+KAUSTUBH24SHATDHAR@users.noreply.github.com>
2023-08-24 21:17:12 +0530

    Time: 2 ms (75.79%), Space: 6.6 MB (52.88%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+
+        int pos=s.find(part);
+
+        while(pos!=string::npos){
+            s.erase(pos,part.length());
+
+            pos=s.find(part);
+
+        }
+        
+        return s;
+    }
+};
