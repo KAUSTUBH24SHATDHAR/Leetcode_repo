c8f0a63e2fb22d68d601fc2d9da9c33e9ca1bcaf
KAUSTUBH24SHATDHAR <103636236+KAUSTUBH24SHATDHAR@users.noreply.github.com>
2023-10-03 13:59:51 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (27.67%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -6,7 +6,7 @@ public:
     
     for(int i=0;i<nums.size();i++){
         for(int j=i+1;j<nums.size();j++){
-            if(nums[i]==nums[j] && i<j){
+            if(nums[i]==nums[j] ){
                 count++;
             }
         }
