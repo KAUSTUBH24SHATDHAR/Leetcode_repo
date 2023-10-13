2b4f3abfcb8ed7af9efb07976e60f427350bcd88
KAUSTUBH24SHATDHAR <103636236+KAUSTUBH24SHATDHAR@users.noreply.github.com>
2023-10-14 05:04:07 +0530

    Time: 24 ms (39.67%), Space: 12 MB (11.23%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,37 +1,102 @@
-class Solution {
-public:
-    string removeDuplicates(string s) {
-
-        string ans="";
-
-        int i=0;
-
-        for(i=0;i<s.length();i++){
-
-            if(ans.length()>0){
-
-                if(ans[ans.length()-1]==s[i]){
-                    ans.pop_back();
+class Solution{
+    public:
+    
+        string removeDuplicates(string s){
+            
+            stack<char>st;
+            
+            for(auto ch: s){
+                
+                if(st.empty()){
+                    st.push(ch);
                 }
-
+                
+                else if(!st.empty() && st.top() == ch){
+                    st.pop();
+                }
+                
                 else{
-
-                    ans.push_back(s[i]);
+                    st.push(ch);
                 }
             }
-
-            else{
-
-                ans.push_back(s[i]);
+            
+            string ans;
+            
+            while(!st.empty()){
+                
+                ans+=st.top();
+                st.pop();
             }
+            
+            reverse(ans.begin(),ans.end());
+            
+            return ans;
+        }
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+
+//         string ans="";
+
+//         int i=0;
+
+//         for(i=0;i<s.length();i++){
+//              if(ans.length()>0){
+
+//                 if(ans[ans.length()-1]==s[i]){
+//                     ans.pop_back();
+//                 }
+
+//                 else{
+
+//                     ans.push_back(s[i]);
+//                 }
+//             }
+
+//             else{
+
+//                 ans.push_back(s[i]);
+//             }
 
                
-        }
+//         }
 
-        return ans;
-    }
+//         return ans;
+//     }
      
 
 
 
-};
+//};
