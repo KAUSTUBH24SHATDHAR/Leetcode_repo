21bfedba4e45f29fc06ba3f352e727ecff37eba7
KAUSTUBH24SHATDHAR <103636236+KAUSTUBH24SHATDHAR@users.noreply.github.com>
2023-10-19 18:42:43 +0530

    Time: 3 ms (47.11%), Space: 6.9 MB (7.09%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -2,52 +2,51 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
         
-        stack<int>s1,s2;
-        
-        string q1,q2;
+        stack<char>st1,st2;
+        string str1,str2;
         
         for(int i=0;i<s.size();i++){
             
-            if(s[i]=='#' && !s1.empty()){
-                
-                s1.pop();
-                
+            if(!st1.empty() && s[i]=='#'){
+                st1.pop();
             }
             
-            else if(s[i]!='#'){
+                else if(s[i]!='#'){
+                    
+                     st1.push(s[i]);
+                }
+            
+           
+            
                 
-                s1.push(s[i]);
             }
-        }
         
+         
         for(int i=0;i<t.size();i++){
             
-            if(t[i]=='#' && !s2.empty()){
-                
-                s2.pop();
-                
+            if(!st2.empty() && t[i]=='#'){
+                st2.pop();
             }
             
-            else if(t[i]!='#'){
-                
-                s2.push(t[i]);
+                 else if(t[i]!='#'){
+                    
+                     st2.push(t[i]);
+                }
+            
+        }
+            
+            while(!st1.empty()){
+                str1.push_back(st1.top());
+                st1.pop();
             }
-        }
-        
-        while(!s1.empty()){
             
-            q1.push_back(s1.top());
-            s1.pop();
+            while(!st2.empty()){
+                str2.push_back(st2.top());
+                st2.pop();
+            }
             
-        }
-        
-         while(!s2.empty()){
             
-            q2.push_back(s2.top());
-            s2.pop();
             
-        }
-        
-        return q1==q2;
+        return str1==str2;
     }
 };
